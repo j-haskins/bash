51c88e7c656c06ee726a0e7d1b44903a44f5a573
Jay Haskins <43075457+j-haskins@users.noreply.github.com>
2020-02-29 14:53:38 -0800

    Update CentOS-Base.sh

--- CentOS-Base.sh
+++ CentOS-Base.sh
@@ -41,8 +41,10 @@ vi /etc/aliases
 newaliases
 
 #install qemu/kvm 
-
+dnf install qemu-kvm qemu-img libvirt virt-install libvirt-client
+systemctl start libvirtd
+systemctl enable libvirtd
 #test qemu/kvm one-liner
-
+virt-install --connect qemu:///system -n centos7 --ram 1024 --disk /var/lib/libvirt/images/centos7.img,size=8 --vcpus 1 --os-type linux --os-variant centos7.0 --graphics none --console pty,target_type=serial --location /var/lib/libvirt/images/CentOS-7-x86_64-Minimal-1908.iso --extra-args='console=ttyS0'
 
 
